17dbc0553e28f951d3b8d6df9b3be4cc09a837f2
felix <felixnjuguna443@gmail.com>
2023-10-05 18:21:23 +0300

    Initial commit

---
+++ hello.py
@@ -0,0 +1,40 @@
+def print_hi(name):
+    print(f'{name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    print_hi('Hello World')
+
+i = 2
+j = 9
+
+i, j = j, i
+
+# Hello world
+print(i)
+print(j)
+
+print('  _______')
+print(' /       \\')
+print('/         \\')
+print('\         /')
+print(' \_______/')
+print('')
+print('\         /')
+print(' \_______/')
+print('+----------+')
+print('   _______')
+print('  /        \\')
+print(' /          \\')
+print('|    STOP    |')
+print(' \          /')
+print('  \________/')
+print('')
+print('  _______')
+print(' /       \\')
+print('/         \\')
+print('+----------+')
+
+
+
+
